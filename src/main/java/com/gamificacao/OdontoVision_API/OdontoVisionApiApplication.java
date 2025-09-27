aef3b146b07b49a7b21d6cd880a8657cb6adc5ea
Sabrina da Motta Café <sabis476@gmail.com>
2025-09-27 14:49:05 -0300

    estrutura do projeto

---
+++ src/main/java/com/gamificacao/OdontoVision_API/OdontoVisionApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamificacao.OdontoVision_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoVisionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontoVisionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamificacao/OdontoVision_API/OdontoVisionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamificacao.OdontoVision_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoVisionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
