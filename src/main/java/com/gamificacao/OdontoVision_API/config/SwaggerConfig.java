2718bda9caf61e94fa51ea16ee19cc0073483d73
Sabrina da Motta Café <sabis476@gmail.com>
2025-09-28 19:42:10 -0300

    swagger

--- src/main/java/com/gamificacao/OdontoVision_API/config/SwaggerConfig.java
+++ src/main/java/com/gamificacao/OdontoVision_API/config/SwaggerConfig.java
@@ -1,26 +1,29 @@
 package com.gamificacao.OdontoVision_API.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "OdontoVision API",
+                version = "v1",
+                description = "API de Gamificação / OdontoVision"
+        )
+)
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI odontoVisionOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("OdontoVision API")
-                        .description("API de Gamificação / OdontoVision")
-                        .version("v1")
-                        .license(new License().name("MIT").url("https://opensource.org/licenses/MIT"))
-                )
-                .externalDocs(new ExternalDocumentation()
-                        .description("Swagger UI")
-                        .url("/swagger-ui/index.html"));
+    public GroupedOpenApi apiV1() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                // **apenas** os seus controllers
+                .packagesToScan("com.gamificacao.OdontoVision_API.controller")
+                // **apenas** rotas da sua API
+                .pathsToMatch("/api/**")
+                .build();
     }
 }
